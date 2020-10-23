0906c668846a147edf65685514673df3c7caf801
RenoLooijmans <73367951+RenoLooijmans@users.noreply.github.com>
2020-10-24 00:22:17 +0200

    Remove config and database

--- src/ShoppingcartServiceProvider.php
+++ src/ShoppingcartServiceProvider.php
@@ -17,19 +17,10 @@ class ShoppingcartServiceProvider extends ServiceProvider
     {
         $this->app->bind('cart', 'Gloudemans\Shoppingcart\Cart');
 
-        $config = __DIR__.'/Config/cart.php';
-        $this->mergeConfigFrom($config, 'cart');
-
-        $this->publishes([__DIR__.'/Config/cart.php' => config_path('cart.php')], 'config');
-
         $this->app['events']->listen(Logout::class, function () {
             if ($this->app['config']->get('cart.destroy_on_logout')) {
                 $this->app->make(SessionManager::class)->forget('cart');
             }
         });
-
-        $this->publishes([
-            realpath(__DIR__.'/Database/migrations') => $this->app->databasePath().'/migrations',
-        ], 'migrations');
     }
 }
